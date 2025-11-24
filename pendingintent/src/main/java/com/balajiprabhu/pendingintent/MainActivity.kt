f9cd375dbc485e56cf1fab3e7ecc3534fab62798
Balaji <balajiprabhu008@gmail.com>
2025-11-24 11:19:52 +0530

    pending intent init commit

---
+++ pendingintent/src/main/java/com/balajiprabhu/pendingintent/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.balajiprabhu.pendingintent
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.balajiprabhu.pendingintent.ui.theme.AndroidRefreshTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AndroidRefreshTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    AndroidRefreshTheme {
+        Greeting("Android")
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,3 +23,4 @@ rootProject.name = "AndroidRefresh"
 include(":app")
 include(":intent")
 include(":services")
+include(":pendingintent")
